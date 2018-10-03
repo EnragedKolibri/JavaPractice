c2a5246b2e1a2fdb873cd5b0d1c37266a16b4f25
EnragedKolibri <enragedkolibri@gmail.com>
2018-10-04 01:24:33 +0300

    spring

---
+++ src/main/java/com/example/inrt/SprInroApplication.java
@@ -0,0 +1,12 @@
+package com.example.inrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprInroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprInroApplication.class, args);
+    }
+}
